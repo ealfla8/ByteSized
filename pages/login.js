7a953a2fafee3d9b0ef97d20ff397a62a51d7d35
Emily Lu <ealfla8@gmail.com>
2022-11-01 23:11:37 -0400

    Added login page, registration page

---
+++ pages/login.js
@@ -0,0 +1,89 @@
+/*export default function Login() {
+	return <div>
+		User Login 
+	</div>
+} */ 
+
+/*https://www.simplilearn.com/tutorials/reactjs-tutorial/login-page-in-reactjs*/
+
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+
+import Button from "react-bootstrap/Button";
+
+import "./login.js";
+
+export default function Login() {
+
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+
+    return email.length > 0 && password.length > 0;
+
+  }
+
+  function handleSubmit(event) {
+
+    event.preventDefault();
+
+  }
+
+  return (
+
+    <div className="Login">
+
+      User Login
+
+      <Form onSubmit={handleSubmit}>
+
+        <Form.Group size="lg" controlId="email">
+
+          <Form.Label>Email</Form.Label>
+
+          <Form.Control
+
+            autoFocus
+
+            type="email"
+
+            value={email}
+
+            onChange={(e) => setEmail(e.target.value)}
+
+          />
+
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="password">
+
+          <Form.Label>Password</Form.Label>
+
+          <Form.Control
+
+            type="password"
+
+            value={password}
+
+            onChange={(e) => setPassword(e.target.value)}
+
+          />
+
+        </Form.Group>
+
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+
+          Login
+
+        </Button>
+
+      </Form>
+
+    </div>
+
+  );
+
+}
---
+++ pages/registration.js
@@ -0,0 +1,5 @@
+export default function Registration() {
+	return <div>
+		User Registration 
+	</div>
+} 
