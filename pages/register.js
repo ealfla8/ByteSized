56ff43f9a4d91d3bd90ba1e7e0e6107cbba61497
Noah Bresler <noahbresler2002@gmail.com>
2022-11-05 17:44:29 -0400

    Add account_type metadata to user signup

--- pages/register.js
+++ pages/register.js
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+
 import {
   FormControl,
   FormLabel,
@@ -10,6 +12,7 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 export default function Register() {
   const supabase = useSupabaseClient();
+  const router = useRouter();
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +23,11 @@ export default function Register() {
       .signUp({
         email: formData.get("email"),
         password: formData.get("password"),
+        options: {
+          data: {
+            account_type: "user",
+          },
+        },
       })
       .then((user, error) => {
         router.push("/home");
