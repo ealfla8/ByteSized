ec6b56211dd1b897fd7f512d748c78a96e643c69
Noah Bresler <noahbresler2002@gmail.com>
2022-11-05 18:18:11 -0400

    Log supabase user in /home

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,10 @@
-export default function Register() {
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+
+export default function Home() {
+  const supabase = useSupabaseClient();
+  const user = useUser();
+
+  console.log(user);
+
   return <div>User home</div>;
 }
