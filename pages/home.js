9f8d22d5df39ebadf49407ecbf5deb2a1e6ff273
michellez3 <michellezou02@gmail.com>
2022-11-07 18:41:17 -0500

    home page layout

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,6 @@
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Box, Heading, Text, Stack, HStack} from "@chakra-ui/react"
 
 export default function Home() {
   const supabase = useSupabaseClient();
@@ -7,7 +8,48 @@ export default function Home() {
 
   console.log(user);
 
-  return <div>User home</div>;
+  return (
+    <Box
+      as="section"
+      pt='90px'
+      pb='198px'
+      px='32px'
+    >
+      <center>
+      <Heading fontSize = '70px'>
+        Welcome to <i>A Second Chance.</i>
+      </Heading>
+      <Text fontSize = '40px' pt="20px">
+        Upcoming drops:
+      </Text>
+      </center>
+      <HStack spacing="50px" pt="70px">
+        <Box bg='gray' w="400px" h="200px">
+          <center>
+          <Text fontSize = '40px' pt="16px">
+          Taco Shells $3.99
+          </Text>
+          </center>
+        </Box>
+        <Box bg='gray' w="400px" h="200px">
+          <center>
+          <Text fontSize = '40px' pt="16px">
+          Taco Shells $3.99
+          </Text>
+          </center>
+        </Box>
+        <Box bg='gray' w="400px" h="200px">
+          <center>
+          <Text fontSize = '40px' pt="16px">
+          Taco Shells $3.99
+          </Text>
+          </center>
+        </Box>
+      </HStack>
+    
+    </Box>
+    
+  );
 }
 
 export async function getServerSideProps(ctx) {
