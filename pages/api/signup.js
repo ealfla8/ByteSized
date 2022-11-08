8f58e1d179500e6c4ddee735c59402a024cc5b43
dyannya <janiarfernan@gmail.com>
2022-11-08 13:46:44 -0500

    fixed formatting

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { createClient } from '@supabase/supabase-js'
+import {createClient} from '@supabase/supabase-js'
 
 export default async function handler(req, res) {
     const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     });
 
     if (error) {
-        res.status(403).json({ error });
+        res.status(403).json({error});
     } else {
         const {error: supabaseError} = await supabase.from('users').insert({id: data.user.id, email: bodyData.email});
         res.status(200).end();
