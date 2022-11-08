ae5469d99dbbcfcaa9dfb0c7152abd78cafe830b
dyannya <janiarfernan@gmail.com>
2022-11-08 16:35:48 -0500

    added register and forgot pwd buttons

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,13 @@
 import {useRouter} from "next/router";
 import {
     FormControl,
-    FormLabel,
     Input,
-    Container,
     Flex,
     Button,
     Heading,
-    Center,
     VStack,
-    InputGroup
+    Text,
+    HStack
 } from "@chakra-ui/react";
 import {useSupabaseClient} from "@supabase/auth-helpers-react";
 
@@ -24,8 +22,7 @@ export default function Login() {
 
         supabase.auth
             .signInWithPassword({
-                email: formData.get("email"),
-                password: formData.get("password"),
+                email: formData.get("email"), password: formData.get("password"),
             })
             .then((user, error) => {
                 // If login successful
@@ -40,45 +37,50 @@ export default function Login() {
             });
     };
 
-    return (
-        <Flex alignItems={"center"} justifyContent={"center"} minH={"100vh"} height={"100vh"}>
-            <Flex
-                direction={"column"}
-                background={"gray.700"}
-                padding={10}
-                rounded={50}
-                position={"relative"}
-                width={"50%"}
-            >
-                <VStack spacing={2} marginTop={5}>
-                    <Heading color="white" size="2xl">A Second Chance</Heading>
-                    <Heading color={"white"} size="md">User Login</Heading>
-                    <FormControl as="form" onSubmit={onSubmit}>
-                        <VStack spacing={5} margin={3}>
-                            <Input
-                                placeholder="email"
-                                type="email"
-                                id="input-email"
-                                name="email"
-                                width={"80%"}
-                                bgColor={"white"}
-                                variant={"outline"}
-                                marginTop={7}
-                            />
-                            <Input
-                                placeholder="password"
-                                type="password"
-                                id="input-password"
-                                name="password"
-                                width={"80%"}
-                                bgColor={"white"}
-                                variant={"outline"}
-                            />
-                            <Button colorScheme={"gray"} variant={"solid"} type="submit">Login</Button>
-                        </VStack>
-                    </FormControl>
-                </VStack>
-            </Flex>
+    return (<Flex alignItems={"center"} justifyContent={"center"} minH={"100vh"} height={"100vh"}>
+        <Flex
+            direction={"column"}
+            background={"gray.700"}
+            padding={7}
+            rounded={50}
+            position={"relative"}
+            width={"50%"}
+        >
+            <VStack spacing={5} marginTop={5}>
+                <Heading color={"white"} size={"2xl"}>A Second Chance</Heading>
+                <Heading color={"white"} size={"md"}>User Login</Heading>
+                <FormControl as="form" onSubmit={onSubmit}>
+                    <VStack spacing={5} margin={3}>
+                        <Input
+                            placeholder={"email"}
+                            type={"email"}
+                            id={"input-email"}
+                            name={"email"}
+                            width={"70%"}
+                            bgColor={"white"}
+                            variant={"outline"}
+                            marginTop={5}
+                        />
+                        <Input
+                            placeholder={"password"}
+                            type={"password"}
+                            id={"input-password"}
+                            name={"password"}
+                            width={"70%"}
+                            bgColor={"white"}
+                            variant={"outline"}
+                        />
+                        <Button colorScheme={"gray"} fontWeight={"bold"} variant={"solid"} textColor={"gray.700"}
+                                type="submit" size={"md"}
+                                width={"30%"}>Login</Button>
+                        <HStack>
+                            <Button colorScheme={"gray"} fontSize={"sm"} variant={"link"}>Forgot Password</Button>
+                            <Text fontWeight={"semibold"} color={"white"}>|</Text>
+                            <Button colorScheme={"gray"} fontSize={"sm"} variant={"link"}>Register</Button>
+                        </HStack>
+                    </VStack>
+                </FormControl>
+            </VStack>
         </Flex>
-    );
+    </Flex>);
 }
