db7c10f6ed2318a60845113e5e554047100e251d
dyannya <janiarfernan@gmail.com>
2022-11-08 23:53:33 -0500

    updated field input labels

--- pages/register.js
+++ pages/register.js
@@ -7,12 +7,53 @@ import {
     Text,
     VStack,
     Heading,
-    Select
+    Select,
+    extendTheme,
+    ChakraProvider,
+    FormLabel,
 } from "@chakra-ui/react";
 import {useRouter} from "next/router";
 import {useState} from "react";
 import {useSupabaseClient} from "@supabase/auth-helpers-react";
 import {error} from "next/dist/build/output/log";
+
+const activeLabelStyles = {
+    transform: "scale(0.85) translateY(-24px)"
+};
+export const theme = extendTheme({
+    components: {
+        Form: {
+            variants: {
+                floating: {
+                    container: {
+                        _focusWithin: {
+                            label: {
+                                ...activeLabelStyles
+                            }
+                        },
+                        "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label, textarea:not(:placeholder-shown) ~ label": {
+                            ...activeLabelStyles
+                        },
+                        label: {
+                            top: 0,
+                            left: 0,
+                            zIndex: 2,
+                            position: "absolute",
+                            backgroundColor: "gray.50",
+                            textColor: "gray.700",
+                            pointerEvents: "none",
+                            fontWeight: "semibold",
+                            mx: 3,
+                            px: 1,
+                            my: 2,
+                            transformOrigin: "left top"
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
 
 export default function Register() {
     const [errorMessage, setErrorMessage] = useState("");
@@ -42,77 +83,101 @@ export default function Register() {
         })
     };
 
-    return (<Flex alignItems="center" minH="100vh" h="100%">
-            <Container
-                direction={"column"}
-                background={"gray.700"}
-                padding={7}
-                rounded={50}
-                position={"relative"}
-                width={"50%"}
-            >
-                <VStack spacing={5} marginTop={5}>
-                    <Heading color={"white"} size={"xl"}>A Second Chance</Heading>
-                    <Heading color={"white"} size={"md"}>User Registration</Heading>
-                    <FormControl as="form" onSubmit={onSubmit}>
-                        <VStack spacing={5} margin={3}>
-                            <Select
-                                placeholder={"Register as:"}
-                                required
-                                width={"80%"}
-                                bgColor={"white"}
-                                variant={"outline"}
+    return (
+        <ChakraProvider theme={theme}>
+            <Flex backgroundColor="gray.700" alignItems="center" minH="100vh" h="100%">
+                <Container
+                    direction="column"
+                    background="gray.50"
+                    padding={7}
+                    rounded={50}
+                    position="relative"
+                    width="50%"
+                    textColor="gray.700"
+                >
+                    <VStack spacing={7} marginTop={5} marginBottom={5}>
+                        <Heading size="xl">A Second Chance</Heading>
+                        <Heading size="md">User Registration</Heading>
+                        <VStack marginTop={5} spacing={7} width="70%">
+                            <FormControl
+                                as="form"
+                                onSubmit={onSubmit}
+                                id="input-category"
+                                variant="floating"
                             >
-                                <option value={"customer"}>Customer</option>
-                                <option value={"store"}>Store</option>
-                            </Select>
-                            <Input
-                                placeholder={"Name"}
-                                type={"name"}
-                                id={"input-name"}
-                                name={"name"}
-                                required
-                                width={"80%"}
-                                bgColor={"white"}
-                                variant={"outline"}
-                            />
-                            <Input
-                                placeholder={"Email"}
-                                type={"email"}
-                                id={"input-email"}
-                                name={"email"}
-                                required
-                                width={"80%"}
-                                bgColor={"white"}
-                                variant={"outline"}
-                            />
-                            <Input
-                                placeholder={"Username"}
-                                type={"username"}
-                                id={"input-username"}
-                                name={"username"}
-                                required
-                                width={"80%"}
-                                bgColor={"white"}
-                                variant={"outline"}
-                            />
-                            <Input placeholder={"Password"}
-                                   type={"password"}
-                                   id={"input-password"}
-                                   name={"password"}
-                                   required
-                                   width={"80%"}
-                                   bgColor={"white"}
-                                   variant={"outline"}
-                            />
+                                <Select
+                                    placeholder="Choose one:"
+                                    required
+                                >
+                                    <option value="customer">Customer</option>
+                                    <option value="store">Store</option>
+                                </Select>
+                                <FormLabel>Register as</FormLabel>
+                            </FormControl>
+                            <FormControl
+                                as="form"
+                                onSubmit={onSubmit}
+                                id="input-name"
+                                variant="floating"
+                            >
+                                <Input
+                                    placeholder=" "
+                                    type="name"
+                                    name="name"
+                                    required
+                                />
+                                <FormLabel>Name</FormLabel>
+                            </FormControl>
+                            <FormControl
+                                as="form"
+                                onSubmit={onSubmit}
+                                variant="floating"
+                                id="input-email"
+                            >
+                                <Input
+                                    placeholder=" "
+                                    type="email"
+                                    name="email"
+                                    required
+                                />
+                                <FormLabel>Email</FormLabel>
+                            </FormControl>
+                            <FormControl
+                                as="form"
+                                onSubmit={onSubmit}
+                                id="input-username"
+                                variant="floating"
+                            >
+                                <Input
+                                    placeholder=" "
+                                    type="username"
+                                    id="input-username"
+                                    name="username"
+                                    required
+                                />
+                                <FormLabel>Username</FormLabel>
+                            </FormControl>
+                            <FormControl
+                                as="form"
+                                onSubmit={onSubmit}
+                                id="input-password"
+                                variant="floating"
+                            >
+                                <Input placeholder=" "
+                                       type="password"
+                                       name="password"
+                                       required
+                                />
+                                <FormLabel>Password</FormLabel>
+                            </FormControl>
                             <Button type="submit">Create Account</Button>
                         </VStack>
-                    </FormControl>
-                </VStack>
-                <Text color={"white"}>
-                    {errorMessage}
-                </Text>
-            </Container>
-        </Flex>
+                    </VStack>
+                    <Text color="white">
+                        {errorMessage}
+                    </Text>
+                </Container>
+            </Flex>
+        </ChakraProvider>
     );
 }
