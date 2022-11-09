23e9408d3690b4a9bd063951dca55e5378ab1b67
dyannya <janiarfernan@gmail.com>
2022-11-08 23:17:43 -0500

    updated input labels

--- pages/login.js
+++ pages/login.js
@@ -7,9 +7,50 @@ import {
     Heading,
     VStack,
     Text,
-    HStack
+    HStack,
+    extendTheme,
+    ChakraProvider,
+    FormLabel,
+    Container
 } from "@chakra-ui/react";
 import {useSupabaseClient} from "@supabase/auth-helpers-react";
+
+const activeLabelStyles = {
+    transform: "scale(0.85) translateY(-24px)"
+};
+export const theme = extendTheme({
+    components: {
+        Form: {
+            variants: {
+                floating: {
+                    container: {
+                        _focusWithin: {
+                            label: {
+                                ...activeLabelStyles
+                            }
+                        },
+                        "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label, textarea:not(:placeholder-shown) ~ label": {
+                            ...activeLabelStyles
+                        },
+                        label: {
+                            top: 0,
+                            left: 0,
+                            zIndex: 2,
+                            position: "absolute",
+                            backgroundColor: "gray.50",
+                            textColor: "gray.700",
+                            pointerEvents: "none",
+                            mx: 3,
+                            px: 1,
+                            my: 2,
+                            transformOrigin: "left top"
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
 
 export default function Login() {
     const router = useRouter();
@@ -37,50 +78,66 @@ export default function Login() {
             });
     };
 
-    return (<Flex alignItems={"center"} justifyContent={"center"} minH={"100vh"} height={"100vh"}>
-        <Flex
-            direction={"column"}
-            background={"gray.700"}
-            padding={7}
-            rounded={50}
-            position={"relative"}
-            width={"50%"}
-        >
-            <VStack spacing={5} marginTop={5}>
-                <Heading color={"white"} size={"2xl"}>A Second Chance</Heading>
-                <Heading color={"white"} size={"md"}>User Login</Heading>
-                <FormControl as="form" onSubmit={onSubmit}>
-                    <VStack spacing={5} margin={3}>
-                        <Input
-                            placeholder={"Email"}
-                            type={"email"}
-                            id={"input-email"}
-                            name={"email"}
-                            width={"70%"}
-                            bgColor={"white"}
-                            variant={"outline"}
-                            marginTop={5}
-                        />
-                        <Input
-                            placeholder={"Password"}
-                            type={"password"}
-                            id={"input-password"}
-                            name={"password"}
-                            width={"70%"}
-                            bgColor={"white"}
-                            variant={"outline"}
-                        />
-                        <Button colorScheme={"gray"} fontWeight={"bold"} variant={"solid"} textColor={"gray.700"}
-                                type="submit" size={"md"}
-                                width={"30%"}>Login</Button>
+    return (
+        <ChakraProvider theme={theme}>
+            <Flex background={"gray.700"} alignItems="center" justifyContent="center" minH="100vh" height="100vh">
+                <Container
+                    direction="column"
+                    background={"gray.50"}
+                    padding={7}
+                    rounded={50}
+                    position="relative"
+                    width="50%"
+                    textColor="gray.700"
+                >
+                    <VStack spacing={5} marginTop={7}>
+                        <Heading size="2xl">A Second Chance</Heading>
+                        <Heading size="md">User Login</Heading>
+                        <FormControl
+                            as="form"
+                            onSubmit={onSubmit}
+                            variant="floating"
+                            width="70%"
+                        >
+                            <Input
+                                placeholder=" "
+                                type="email"
+                                id="input-email"
+                                name="email"
+                            />
+                            <FormLabel>Email</FormLabel>
+                        </FormControl>
+                        <FormControl
+                            as="form"
+                            onSubmit={onSubmit}
+                            variant="floating"
+                            width="70%"
+                        >
+                            <FormLabel>Email</FormLabel>
+                            <Input
+                                placeholder=" "
+                                type="password"
+                                id="input-password"
+                                name="password"
+                            />
+                            <FormLabel>Password</FormLabel>
+                        </FormControl>
+                        <Button
+                            colorScheme="gray"
+                            type="submit"
+                            size="md"
+                            width="30%"
+                        >
+                            Login
+                        </Button>
                         <HStack>
-                            <Button colorScheme={"gray"} fontSize={"sm"} variant={"link"}>Forgot Password</Button>
-                            <Text fontWeight={"semibold"} color={"white"}>|</Text>
-                            <Button colorScheme={"gray"} fontSize={"sm"} variant={"link"}>Register</Button>
+                            <Button fontSize="sm" variant="link">Forgot Password</Button>
+                            <Text color="gray.400" fontWeight="semibold">|</Text>
+                            <Button fontSize="sm" variant="link">Register</Button>
                         </HStack>
                     </VStack>
-                </FormControl>
-            </VStack>
-        </Flex>
-    </Flex>);
+                </Container>
+            </Flex>
+        </ChakraProvider>
+    );
 }
