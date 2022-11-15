0b6936544673f7ffdf69e6644e13c0c501a60ca6
michellez3 <michellezou02@gmail.com>
2022-11-15 16:33:35 -0500

    authorization for restaruant

--- pages/restaurant/index.js
+++ pages/restaurant/index.js
@@ -1,4 +1,5 @@
-import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
 export default function Restaurant() {
   const supabase = useSupabaseClient();
@@ -7,4 +8,44 @@ export default function Restaurant() {
   console.log(user);
 
   return <div>Restaurant home</div>;
+}
+
+export async function getServerSideProps(ctx) {
+  const supabase = createServerSupabaseClient(ctx);
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+  console.log(session);
+
+  if (error) throw error;
+  if (!session)
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+
+  // Retrieve provider_token & logged in user's third-party id from metadata
+  const { provider_token, user } = session;
+
+  console.log(user);
+  if (user.user_metadata.account_type == "restaurant") {
+    return {
+      props: {},
+    };
+  } else if (user.user_metadata.account_type == "user") {
+    return {
+      redirect: {
+        destination: "/home/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }
