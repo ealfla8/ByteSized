61e1323f16e2fd88f0f4a155e29a830828a47cd6
Emily Lu <ealfla8@gmail.com>
2022-11-18 23:05:55 -0500

    added buttons and columns to restaurant profile

--- pages/restaurant/RestaurantView.js
+++ pages/restaurant/RestaurantView.js
@@ -1,33 +1,60 @@
 import { Image, Input, Button, Divider, Heading } from "@chakra-ui/react";
+import {
+  SimpleGrid,
+  Grid,
+  GridItem,
+  Box,
+  Flex,
+  Spacer,
+  Center,
+  Square,
+  Text,
+} from "@chakra-ui/react";
 
 export default function RestaurantView({ restaurant }) {
   console.log(restaurant);
 
   return (
     <div>
-      <Heading>{restaurant.email}</Heading>
+      <Grid h="100vh" templateColumns="500px 1fr" gap={4}>
+        <GridItem bg="papayawhip">
+          <Center>
+            <Heading>Restaurant Name</Heading>
+          </Center>
+          <Center w="500px" marginTop="50px">
+            <Image
+              boxSize="200px"
+              objectFit="cover"
+              src="https://st.depositphotos.com/1064950/1282/i/950/depositphotos_12829992-stock-photo-restaurant-signage.jpg"
+              alt="Example Restaurant Picture"
+            />
+          </Center>
 
-      <Image
-        boxSize="200px"
-        objectFit="cover"
-        src="https://st.depositphotos.com/1064950/1282/i/950/depositphotos_12829992-stock-photo-restaurant-signage.jpg"
-        alt="Example Restaurant Picture"
-      />
+          <Flex color="white" flexDirection="column" alignItems="center">
+            <Center w="250px" h="50px" marginTop="50px">
+              <Button borderRightRadius="0" color="tomato">
+                Edit Profile
+              </Button>
+            </Center>
 
-      <Divider
-        orientation="horizontal" /*the divider is because I don't know how to add spacing*/
-      />
+            <Center w="250px" h="50px" marginTop="50px">
+              <Button borderRightRadius="0" color="tomato">
+                Followers
+              </Button>
+            </Center>
 
-      <Button colorScheme="blue">Edit Profile</Button>
-      <Divider orientation="horizontal" />
+            <Center w="250px" h="50px" marginTop="50px">
+              <Button borderRightRadius="0" color="tomato">
+                <Text>Write a post..</Text>
+              </Button>
+            </Center>
+          </Flex>
 
-      <Button colorScheme="blue">Followers</Button>
-      <Divider orientation="horizontal" />
-      <Button colorScheme="blue">Write a post..</Button>
-      <Divider orientation="horizontal" />
-      <Divider orientation="horizontal" />
-      <Input placeholder="Type your post here!" size="md" />
-      <Button colorScheme="blue">Post</Button>
+          {/* <Heading>{restaurant.email}</Heading> */}
+        </GridItem>
+
+        <GridItem bg="tomato">right col</GridItem>
+      </Grid>
     </div>
   );
 }
