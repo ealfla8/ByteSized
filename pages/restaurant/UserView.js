e60f5aca4c079f7b3124174dbf78642bdb5cca93
michellez3 <michellezou02@gmail.com>
2022-11-20 18:16:47 -0500

    profile pic

--- pages/restaurant/UserView.js
+++ pages/restaurant/UserView.js
@@ -1,3 +1,23 @@
-export default function UserView() {
-  return <div>User View</div>;
+import { Grid, GridItem, Flex, Box, Image } from "@chakra-ui/react";
+
+export default function UserView({ restaurant }) {
+  console.log(restaurant);
+
+  return (
+    <Grid templateColumns="500px 1fr" minH={"100vh"}>
+      <GridItem h="100%">
+        <Flex flexDirection="column" alignItems="center">
+          <Image
+            src={restaurant.profile_pic}
+            h="150px"
+            w="150px"
+            marginTop={"20px"}
+          ></Image>
+          <Box>hi2</Box>
+          <Box>hi3</Box>
+        </Flex>
+      </GridItem>
+      <GridItem h="100%" bg="green" />
+    </Grid>
+  );
 }
