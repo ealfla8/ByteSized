f5de9eb8d42bf406a2977448ae3cea6c825c8adb
Emily Lu <ealfla8@gmail.com>
2022-11-29 20:26:54 -0500

    Edited restaurant profile and create new deal info

--- pages/restaurant/RestaurantView.js
+++ pages/restaurant/RestaurantView.js
@@ -1,4 +1,11 @@
-import { Image, Input, Button, Divider, Heading } from "@chakra-ui/react";
+import {
+  Image,
+  Input,
+  Button,
+  Divider,
+  Heading,
+  FormControl,
+} from "@chakra-ui/react";
 import {
   SimpleGrid,
   Grid,
@@ -9,19 +16,38 @@ import {
   Center,
   Square,
   Text,
+  Stack,
 } from "@chakra-ui/react";
 
 export default function RestaurantView({ restaurant }) {
-  console.log(restaurant);
+  function onCreateClick(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    console.log(formData.get("itemName"));
+
+    fetch("/api/create-new-deal", {
+      method: "POST",
+      body: JSON.stringify({
+        itemName: formData.get("itemName"),
+        oldPrice: formData.get("oldPrice"),
+        newPrice: formData.get("newPrice"),
+        quantity: formData.get("quantity"),
+        id: restaurant.id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
 
   return (
     <div>
       <Grid h="100vh" templateColumns="500px 1fr" gap={4}>
         <GridItem bg="papayawhip">
           <Center>
-            <Heading>Restaurant Name</Heading>
+            <Heading> {restaurant.name}</Heading>
           </Center>
-          <Center w="500px" marginTop="50px">
+          <Center>
             <Image
               boxSize="200px"
               objectFit="cover"
@@ -29,23 +55,20 @@ export default function RestaurantView({ restaurant }) {
               alt="Example Restaurant Picture"
             />
           </Center>
+          <Center>
+            <Text> {restaurant.address} </Text>
+          </Center>
 
           <Flex color="white" flexDirection="column" alignItems="center">
             <Center w="250px" h="50px" marginTop="50px">
-              <Button borderRightRadius="0" color="tomato">
+              <Button borderRightRadius="0" color="black">
                 Edit Profile
               </Button>
             </Center>
 
             <Center w="250px" h="50px" marginTop="50px">
-              <Button borderRightRadius="0" color="tomato">
+              <Button borderRightRadius="0" color="black">
                 Followers
-              </Button>
-            </Center>
-
-            <Center w="250px" h="50px" marginTop="50px">
-              <Button borderRightRadius="0" color="tomato">
-                <Text>Write a post..</Text>
               </Button>
             </Center>
           </Flex>
@@ -53,7 +76,21 @@ export default function RestaurantView({ restaurant }) {
           {/* <Heading>{restaurant.email}</Heading> */}
         </GridItem>
 
-        <GridItem bg="tomato">right col</GridItem>
+        <GridItem bg="tomato">
+          <Center>
+            <Stack spacing={3}>
+              <FormControl as="form" onSubmit={onCreateClick}>
+                <Input placeholder="Item Name" size="md" name="itemName" />
+                <Input placeholder="Old Price" size="md" name="oldPrice" />
+                <Input placeholder="New Price" size="md" name="newPrice" />
+                <Input placeholder="Quantity" size="md" name="quantity" />
+                <Center>
+                  <Button type="submit">Create this deal!</Button>
+                </Center>
+              </FormControl>
+            </Stack>
+          </Center>
+        </GridItem>
       </Grid>
     </div>
   );
