2ce51187be190601c069534172130b91a69db3d6
dyannya <janiarfernan@gmail.com>
2022-12-06 10:24:47 -0500

    updated restaurant view home

--- pages/restaurant/RestaurantView.js
+++ pages/restaurant/RestaurantView.js
@@ -2,21 +2,14 @@ import {
   Image,
   Input,
   Button,
-  Divider,
   Heading,
   FormControl,
-} from "@chakra-ui/react";
-import {
-  SimpleGrid,
+  FormLabel,
   Grid,
   GridItem,
-  Box,
   Flex,
-  Spacer,
   Center,
-  Square,
-  Text,
-  Stack,
+  VStack,
 } from "@chakra-ui/react";
 
 export default function RestaurantView({ restaurant }) {
@@ -43,57 +36,111 @@ export default function RestaurantView({ restaurant }) {
   }
 
   return (
-    <div>
-      <Grid h="100vh" templateColumns="500px 1fr" gap={4}>
-        <GridItem bg="papayawhip">
-          <Center>
-            <Heading> {restaurant.name}</Heading>
-          </Center>
-          <Center>
-            <Image
-              boxSize="200px"
-              objectFit="cover"
-              src="https://st.depositphotos.com/1064950/1282/i/950/depositphotos_12829992-stock-photo-restaurant-signage.jpg"
-              alt="Example Restaurant Picture"
-            />
-          </Center>
-          <Center>
-            <Text> {restaurant.address} </Text>
-          </Center>
+    <Flex
+        bg="gray.700"
+        as="section"
+        pt={10}
+        pb={10}
+        minH="100vh"
+        height="100vh"
+        justifyContent="center"
+        alignItems="center"
+        spacing={5}
+    >
+      <VStack>
+        <Heading size="3xl" color="gray.50">A Second Chance</Heading>
+        <Heading color="gray.50">· · ────────────── ·𖥸· ────────────── · ·</Heading>
 
-          <Flex color="white" flexDirection="column" alignItems="center">
-            <Center w="250px" h="50px" marginTop="50px">
-              <Button borderRightRadius="0" color="black">
-                Edit Profile
-              </Button>
-            </Center>
+        <Grid
+            templateAreas={`"nav main"`}
+            gridTemplateRows={'100% 1fr 60%'}
+            gridTemplateColumns={'40% 1fr'}
+            h="100%"
+            w="100%"
+            gap={5}
+        >
+          <GridItem bg="gray.500" area={"nav"} rounded={40}>
+            <VStack spacing={7} mt={5} mb={5}>
+                <Heading size="xl"> {restaurant.name}</Heading>
+                <Image
+                  boxSize="200px"
+                  objectFit="cover"
+                  src="https://st.depositphotos.com/1064950/1282/i/950/depositphotos_12829992-stock-photo-restaurant-signage.jpg"
+                  alt="Example Restaurant Picture"
+                  rounded={15}
+                />
 
-            <Center w="250px" h="50px" marginTop="50px">
-              <Button borderRightRadius="0" color="black">
-                Followers
-              </Button>
-            </Center>
-          </Flex>
+                <Heading size="sm"> {restaurant.address} </Heading>
+                  <Button
+                      colorScheme="gray"
+                      type="submit"
+                      size="md"
+                      width="30%"
+                  >
+                    Edit Profile
+                  </Button>
 
-          {/* <Heading>{restaurant.email}</Heading> */}
-        </GridItem>
+                  <Button
+                      colorScheme="gray"
+                      type="submit"
+                      size="md"
+                      width="30%"
+                  >
+                    Followers
+                  </Button>
 
-        <GridItem bg="tomato">
-          <Center>
-            <Stack spacing={3}>
-              <FormControl as="form" onSubmit={onCreateClick}>
-                <Input placeholder="Item Name" size="md" name="itemName" />
-                <Input placeholder="Old Price" size="md" name="oldPrice" />
-                <Input placeholder="New Price" size="md" name="newPrice" />
-                <Input placeholder="Quantity" size="md" name="quantity" />
-                <Center>
-                  <Button type="submit">Create this deal!</Button>
-                </Center>
-              </FormControl>
-            </Stack>
-          </Center>
-        </GridItem>
-      </Grid>
-    </div>
+            </VStack>
+          </GridItem>
+
+          <GridItem bg="gray.50" area={"main"} rounded={40}>
+            <Flex
+                alignItems="center"
+                justifyContent="center"
+                position="relative"
+                minH="full"
+                height="full"
+            >
+              <Center>
+                  <FormControl as="form" onSubmit={onCreateClick}>
+                    <VStack spacing={7} mt={5} mb={5}>
+                    <FormControl variant="floating">
+                      <Input
+                          placeholder=" "
+                          name="itemName"
+                      />
+                      <FormLabel>Item Name</FormLabel>
+                    </FormControl>
+                    <FormControl variant="floating">
+                      <Input
+                          placeholder=" "
+                          name="oldPrice"
+                      />
+                      <FormLabel>Old Price</FormLabel>
+                    </FormControl>
+                    <FormControl variant="floating">
+                      <Input
+                          placeholder=" "
+                          name="newPrice"
+                      />
+                      <FormLabel>New Price</FormLabel>
+                    </FormControl>
+                    <FormControl variant="floating">
+                      <Input
+                          placeholder=" "
+                          name="quantity"
+                      />
+                      <FormLabel>Quantity</FormLabel>
+                    </FormControl>
+                    <Button type="submit">
+                      Create Deal
+                    </Button>
+                    </VStack>
+                  </FormControl>
+              </Center>
+            </Flex>
+          </GridItem>
+        </Grid>
+      </VStack>
+    </Flex>
   );
 }
